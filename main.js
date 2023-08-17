0e47621d9b8c6946dfa3af87ce72e1451282bbfd
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2023-08-17 12:14:37 -0300

    conectando
    conectando os botoes com o js

--- main.js
+++ main.js
@@ -1,1 +1,52 @@
-alert('olá');
+function tocaSomPon(){
+    document.querySelector('#som_tecla_pom').play();
+}
+const listaDeTeclas = document.querySelectorAll ('.tecla');
+
+listaDeTeclas[0].onclick = tocaSomPon;
+
+
+
+/*
+function tocaSomclap(){
+    document.querySelector('#som_tecla_clap').play();
+}
+document.querySelector ('.tecla_clap').onclick = tocaSomclap;
+
+function tocaSomtim(){
+    document.querySelector('#som_tecla_tim').play();
+}
+document.querySelector ('.tecla_tim').onclick = tocaSomtim;
+
+function tocaSompuff(){
+    document.querySelector('#som_tecla_puff').play();
+}
+document.querySelector ('.tecla_puff').onclick = tocaSompuff;
+
+function tocaSomsplash(){
+    document.querySelector('#som_tecla_splash').play();
+}
+document.querySelector ('.tecla_splash').onclick = tocaSomsplash;
+
+function tocaSomtoim(){
+    document.querySelector('#som_tecla_toim').play();
+}
+document.querySelector ('.tecla_toim').onclick = tocaSomtoim;
+
+function tocaSompsh(){
+    document.querySelector('#som_tecla_psh').play();
+}
+document.querySelector ('.tecla_psh').onclick = tocaSompsh;
+
+function tocaSomtic(){
+    document.querySelector('#som_tecla_tic').play();
+}
+document.querySelector ('.tecla_tic').onclick = tocaSomtic;
+
+function tocaSomtom(){
+    document.querySelector('#som_tecla_tom').play();
+}
+document.querySelector ('.tecla_tom').onclick = tocaSomtom;
+
+*/
+
