5edf599480e7ccaf782e2b52edab0a497c746629
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2023-08-17 18:09:56 -0300

    ativando
    ativando

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio){
+    document.querySelector(seletorAudio).play();
 }
 const listaDeTeclas = document.querySelectorAll ('.tecla');
 
@@ -12,50 +12,10 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++){
     tecla.onclick = function(){
         tocaSom(idAudio);
     }
+    tecla.onkeydown = function() {
+        tecla.classList.add('ativa');
+    }
 }
 
 
-
-/*
-function tocaSomclap(){
-    document.querySelector('#som_tecla_clap').play();
-}
-document.querySelector ('.tecla_clap').onclick = tocaSomclap;
-
-function tocaSomtim(){
-    document.querySelector('#som_tecla_tim').play();
-}
-document.querySelector ('.tecla_tim').onclick = tocaSomtim;
-
-function tocaSompuff(){
-    document.querySelector('#som_tecla_puff').play();
-}
-document.querySelector ('.tecla_puff').onclick = tocaSompuff;
-
-function tocaSomsplash(){
-    document.querySelector('#som_tecla_splash').play();
-}
-document.querySelector ('.tecla_splash').onclick = tocaSomsplash;
-
-function tocaSomtoim(){
-    document.querySelector('#som_tecla_toim').play();
-}
-document.querySelector ('.tecla_toim').onclick = tocaSomtoim;
-
-function tocaSompsh(){
-    document.querySelector('#som_tecla_psh').play();
-}
-document.querySelector ('.tecla_psh').onclick = tocaSompsh;
-
-function tocaSomtic(){
-    document.querySelector('#som_tecla_tic').play();
-}
-document.querySelector ('.tecla_tic').onclick = tocaSomtic;
-
-function tocaSomtom(){
-    document.querySelector('#som_tecla_tom').play();
-}
-document.querySelector ('.tecla_tom').onclick = tocaSomtom;
-
-*/
 
