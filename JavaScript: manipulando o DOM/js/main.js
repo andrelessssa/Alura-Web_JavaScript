9413e7ba806c852b03acaeb04ec0c917b5b1b003
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2023-09-11 17:58:49 -0300

    alterando
    varios componentes

--- JavaScript: manipulando o DOM/js/main.js
+++ JavaScript: manipulando o DOM/js/main.js
@@ -1,40 +1,24 @@
-//console.log('oi');
-
-//const robotron = document.querySelector("#robotron")
 
 const subtrair = document.querySelector("#subtrair")
 const somar = document.querySelector("#somar")
 const braco = document.querySelector("#braco")
 
 const controle = document.querySelectorAll('.controle-ajuste')
-console.log(controle)
 
-somar.addEventListener("click" , (evento) => {
-    manipulaDados('somar')
-    //braco.value = parseInt(braco.value) + 1
-});
+controle.forEach( (elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.textContent)
+    })
 
-subtrair.addEventListener("click" , (evento) => {
-    manipulaDados('subtrair')
-    //braco.value = parseInt(braco.value) - 1
 });
 
 function manipulaDados (operacao) {
-    if (operacao === "subtrair"){
+    if (operacao === "-"){
         braco.value = parseInt(braco.value) - 1
 
-    }else
+    }else{
     braco.value = parseInt(braco.value) + 1
+    }
 }
 
 
-//robotron.addEventListener('click', (evento) => {
-//    console.log(evento)
-//});
-
-//function dizOi (nome) {
-//    console.log("oi " + nome)
-//    console.log("Bem-Vindo")
-//};
-
-//dizOi("André")
