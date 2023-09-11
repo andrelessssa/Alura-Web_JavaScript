c09e53828064e5cfc46f4de6e9572300668f959d
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2023-09-11 17:35:46 -0300

    alterando
    varios componentes

--- JavaScript: manipulando o DOM/js/main.js
+++ JavaScript: manipulando o DOM/js/main.js
@@ -6,14 +6,26 @@ const subtrair = document.querySelector("#subtrair")
 const somar = document.querySelector("#somar")
 const braco = document.querySelector("#braco")
 
+const controle = document.querySelectorAll('.controle-ajuste')
+console.log(controle)
+
 somar.addEventListener("click" , (evento) => {
-    braco.value = parseInt(braco.value) + 1
+    manipulaDados('somar')
+    //braco.value = parseInt(braco.value) + 1
 });
 
 subtrair.addEventListener("click" , (evento) => {
-    braco.value = parseInt(braco.value) - 1
+    manipulaDados('subtrair')
+    //braco.value = parseInt(braco.value) - 1
 });
 
+function manipulaDados (operacao) {
+    if (operacao === "subtrair"){
+        braco.value = parseInt(braco.value) - 1
+
+    }else
+    braco.value = parseInt(braco.value) + 1
+}
 
 
 //robotron.addEventListener('click', (evento) => {
